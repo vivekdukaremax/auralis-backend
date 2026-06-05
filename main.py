95e9c85cbe6e4d3b814437b06b28afe38a3bec1a
Vivek Dukare <vivekdukare001@gmail.com>
2026-06-05 12:53:26 +0530

    Add health endpoint to main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "online"}
+
+@app.get("/health")
+def health():
+    return {
+        "service": "auralis-backend",
+        "status": "healthy"
+    }
